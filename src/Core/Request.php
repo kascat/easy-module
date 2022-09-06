f18418bc4eaed34d6b00dbd2cfd71b21324aa75d
Fabio <fabiojd47@gmail.com>
2022-09-06 18:42:08 -0300

    Core

---
+++ src/Core/Request.php
@@ -0,0 +1,61 @@
+<?php
+
+namespace Kascat\EasyModule\Core;
+
+use Illuminate\Foundation\Http\FormRequest;
+use Illuminate\Contracts\Validation\Validator;
+use Illuminate\Http\Exceptions\HttpResponseException;
+use Illuminate\Http\Response;
+use Illuminate\Support\Str;
+
+/**
+ * Class Request
+ * @package Kascat\EasyModule\Core
+ */
+class Request extends FormRequest
+{
+    /**
+     * Get the validation rules that apply to the request.
+     *
+     * @return array
+     */
+    public function rules()
+    {
+        $method = "validateTo" . Str::ucfirst($this->route()->getActionMethod());
+
+        if (method_exists($this, $method)) {
+            return $this->$method();
+        }
+
+        return [];
+    }
+
+    /**
+     * Determine if the user is authorized to make this request.
+     *
+     * @return bool
+     */
+    public function authorize()
+    {
+        $method = "authorizeTo" . Str::ucfirst($this->route()->getActionMethod());
+
+        if (method_exists($this, $method)) {
+            return $this->$method();
+        }
+
+        return true;
+    }
+
+    /**
+     * Configuration for exception handler
+     *
+     * @param Validator $validator
+     * @return void
+     */
+    protected function failedValidation(Validator $validator)
+    {
+        throw new HttpResponseException(
+            response()->json($validator->errors(), Response::HTTP_UNPROCESSABLE_ENTITY)
+        );
+    }
+}
---
+++ src/Core/Response.php
@@ -0,0 +1,30 @@
+<?php
+
+namespace Kascat\EasyModule\Core;
+
+use Illuminate\Support\Str;
+
+/**
+ * Trait Response
+ * @package Kascat\EasyModule\Core
+ */
+trait Response
+{
+    /**
+     * @param $data
+     * @param int $statusCode
+     * @param array $headers
+     * @param int $options
+     * @return mixed
+     */
+    public function response($data = [], int $statusCode = \Illuminate\Http\Response::HTTP_OK, array $headers = [], $options = 0)
+    {
+        $method = "responseTo" . Str::ucfirst(request()->route()->getActionMethod());
+
+        if (method_exists($this, $method)) {
+            return $this->$method($data, $statusCode, $headers, $options);
+        }
+
+        return \response()->json($data, $statusCode, $headers, $options);
+    }
+}
